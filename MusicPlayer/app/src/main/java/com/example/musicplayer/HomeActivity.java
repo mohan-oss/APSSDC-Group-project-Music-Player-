122488098612c2eb7c6a1ea9a3540189ff0a6621
mohan-oss <mohansurendat.mullapudi@gmail.com>
2021-07-21 11:33:05 +0530

    MusicPlayer

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/HomeActivity.java
@@ -0,0 +1,109 @@
+package com.example.musicplayer;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    //ActivityHomeBinding binding;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //binding = DataBindingUtil.setContentView(this,R.layout.activity_home);
+        listView= findViewById(R.id.listView);
+        auth = FirebaseAuth.getInstance();
+        //binding.textView.setText("Welcome "+auth.getCurrentUser().getEmail());
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                       Toast.makeText(HomeActivity.this, "Runtime permission given", Toast.LENGTH_SHORT).show();
+                        ArrayList<File> mySongs = fetchSongs(Environment.getExternalStorageDirectory());
+                        String [] items = new String[mySongs.size()];
+                        for(int i=0;i<mySongs.size();i++){
+                            items[i] = mySongs.get(i).getName().replace(".mp3", " 🎶");
+                        }
+
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(HomeActivity.this, android.R.layout.simple_list_item_1, items);
+                        listView.setAdapter(adapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Intent intent = new Intent(HomeActivity.this, PlaySong.class);
+                                String currentSong = listView.getItemAtPosition(position).toString();
+                                intent.putExtra("songList", mySongs);
+                                intent.putExtra("currentSong", currentSong);
+                                intent.putExtra("position", position);
+                                startActivity(intent);
+                            }
+                        });
+
+
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+    }
+
+    public ArrayList<File> fetchSongs(File file){
+        ArrayList arrayList = new ArrayList();
+        /*connect your mobile
+        * put light mode*/
+        File [] songs = file.listFiles();
+        if(songs !=null){
+            for(File myFile: songs){
+                if(!myFile.isHidden() && myFile.isDirectory()){
+                    arrayList.addAll(fetchSongs(myFile));
+                }
+                else{
+                    if(myFile.getName().endsWith(".mp3") && !myFile.getName().startsWith(".")){
+                        arrayList.add(myFile);
+                    }
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    public void signout(View view) {
+        auth.signOut();
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
